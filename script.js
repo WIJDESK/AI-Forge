e532113e953625e2a88524b613b84d6b5779b730
WIJDESK <vitalbel321@gmail.com>
2026-09-17 15:22:02 +0000

    Updated

--- script.js
+++ script.js
@@ -122,19 +122,10 @@ window.addEventListener('resize', () => {
     }
     console.log(scrollPos);
   });
+
+  header.addClass("header-hidden");
+  setTimeout(function () {
+    header.addClass("header-visible");
+  }, 100);
 });
 
-  const revealEls = document.querySelectorAll('.reveal');
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target); // animate once
-      }
-    });
-  }, {
-    threshold: 0.15 // trigger when 15% of element is visible
-  });
-
-  revealEls.forEach(el => observer.observe(el));
